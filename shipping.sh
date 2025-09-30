ae3bee58d4a48ca42cef23c037a79b7fa6023897
ABhishekraojupaka <jupaka.abhishek@gmail.com>
2025-09-30 19:33:34 +0530

    variable

--- shipping.sh
+++ shipping.sh
@@ -12,23 +12,24 @@ dnf install mysql -y &>>$LOG_FILE
 
 
 # mysql -h $MYSQL_HOST -uroot -pRoboshop@1 -e 'use cities' &>>$LOG_FILE
-# if [ $? -ne 0 ]; then
-#     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-#     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
-#     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
-# else
-#     echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
-# fi
-
-DB_CHECK=$(mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -N -s -e 'show databases like "cities";' 2>>$LOG_FILE | xargs)
-if [ "$DB_CHECK" = "cities" ]; then
-  echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
+if ! mysql -h $MYSQL_HOST -uroot -pRoboshop@1 -e 'use cities;'; then
+#[ $? -ne 0 ]; then
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
 else
-  echo "Loading Shipping schema..."
-  mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-  mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
-  mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+    echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
 fi
+
+# DB_CHECK=$(mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -N -s -e 'show databases like "cities";' 2>>$LOG_FILE | xargs)
+# if [ "$DB_CHECK" = "cities" ]; then
+#   echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
+# else
+#   echo "Loading Shipping schema..."
+#   mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+#   mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
+#   mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+# fi
 
 app_restart
 print_total_time
