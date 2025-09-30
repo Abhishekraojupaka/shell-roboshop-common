48af8e8bfb026107efab80c0ec172e518cd909be
ABhishekraojupaka <jupaka.abhishek@gmail.com>
2025-09-30 19:03:50 +0530

    variable

--- shipping.sh
+++ shipping.sh
@@ -10,8 +10,9 @@ systemd_setup
 
 dnf install mysql -y &>>$LOG_FILE
 
-mysql -h $MYSQL_HOST -uroot -pRoboshop@1 -e 'use cities' &>>$LOG_FILE
-if [ $? -ne 0 ]; then
+
+mysql=mysql -h $MYSQL_HOST -uroot -pRoboshop@1 -e 'use cities' &>>$LOG_FILE
+if [ $mysql -ne 0 ]; then
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
