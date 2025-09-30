65133b4fa152fd4b75bc6604c6dce350668f6396
ABhishekraojupaka <jupaka.abhishek@gmail.com>
2025-09-30 20:33:36 +0530

    variable

--- shipping.sh
+++ shipping.sh
@@ -12,11 +12,11 @@ dnf install mysql -y &>>$LOG_FILE
 
 
 # mysql -h $MYSQL_HOST -uroot -pRoboshop@1 -e 'use cities' &>>$LOG_FILE
-if ! mysql -h $MYSQL_HOST -uroot -pRoboshop@1 -e 'use cities;'; then
+if ! mysql -h 172.31.21.89 -uroot -pRoboshop@1 -e 'use cities;'; then
 #[ $? -ne 0 ]; then
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+    mysql -h 172.31.21.89 -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h 172.31.21.89 -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
+    mysql -h 172.31.21.89 -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
 else
     echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
 fi
